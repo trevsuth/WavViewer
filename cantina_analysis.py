af76ef3412931b8a9f4a1aa610ec578b52e67407
trevsuth <trevor.sutherland@gmail.com>
2024-05-15 15:45:49 -0400

    taking a break

--- cantina_analysis.py
+++ cantina_analysis.py
@@ -1,34 +1,14 @@
 import librosa
-import librosa.display
-import soundfile as sf
+import matplotlib.pyplot as plt
 
 audio_path = './WavFiles/CantinaBand60.wav'
 # load audio
 y, sr = librosa.load(audio_path)
-# y = numpy array of amplitudes
-# sr = sampling rate.  SR is number of samples taken per second
 
-"""# extract features
-mfccs = librosa.feature.mfcc(y=y, sr=sr)
-plt.figure(figsize=(10, 4))
-librosa.display.specshow(mfccs, x_axis='time')
-plt.colorbar()
-plt.title('MFCC')
-plt.tight_layout()
-plt.show()
-"""
+tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+print(tempo) # in beats per min
+print(beat_frames) #
+# print('Estimated tempo: {:.2f} beats per minute'.format(tempo[0]))
 
-# ALt way to calculate phss
-# Compute the short-time Fourier transform (STFT)
-D = librosa.stft(y)
-# Separate the harmonic and percussive components
-H, P = librosa.decompose.hpss(D)
-# Convert back to time domain
-harmonic = librosa.istft(H)
-percussive = librosa.istft(P)
-#plt.plot(harmonic)
-#plt.plot(percussive)
-#plt.show()
-
-sf.write('./harmonic.wav', harmonic, sr)
-sf.write('./percussive.wav', percussive, sr)
+beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+print(beat_times)
